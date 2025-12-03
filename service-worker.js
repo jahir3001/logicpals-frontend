a467e3377c4ee95fa9bd31d48b8c34f0c2faee07
jahir3001 <jahir3001@gmail.com>
2025-12-03 19:19:33 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// Simple Service Worker for PWA
+const CACHE_NAME = 'logicpals-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/signup.html',
+  '/dashboard.html',
+  '/learn.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
